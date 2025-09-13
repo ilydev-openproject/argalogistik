7e0e9cf2b63d6978a264a046e3d1fa2990de5782
Ilyas Maazib <ilyasmaazib9@gmail.com>
2025-09-14 01:47:55 +0700

    first commit

--- app/currencyHelper.php
+++ app/currencyHelper.php
@@ -6,11 +6,11 @@ if (!function_exists('shortNumber')) {
         if ($num >= 1000000000000) {
             return round($num / 1000000000000, 1) . 'T';
         } elseif ($num >= 1000000000) {
-            return round($num / 1000000000, 1) . 'B';
+            return round($num / 1000000000, 1) . 'M';
         } elseif ($num >= 1000000) {
-            return round($num / 1000000, 1) . 'M';
+            return round($num / 1000000, 1) . 'JT';
         } elseif ($num >= 1000) {
-            return round($num / 1000, 1) . 'K';
+            return round($num / 1000, 1) . 'RB';
         }
 
         return $num;
